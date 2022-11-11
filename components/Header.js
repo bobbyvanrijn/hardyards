4b48cbcc5ceb2ca793c07b6b4084abd96ae2437a
Bobby van Rijn <bobbyvanrijn@gmail.com>
2022-11-11 15:55:17 +0100

    Tweaks: bunch

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,26 @@
 import Image from 'next/image';
+import { RadioButton, CircleSolid } from '@carbon/icons-react';
 import styles from './Header.module.css';
 
 export default function Header() {
     return (
         <>
-            <div className={styles.header} >
+            <div className={styles.header}>
+                <div>
+                    <RadioButton size='40' />
+                    {/* <CircleSolid size='40' /> */}
+                </div>
                 <Image
+                    className={styles.header__logo} 
                     src='/hardyards.svg'
                     width='300'
                     height='40'
                     alt='Hardyards logo'
                 />
+                <div>
+                    {/* <RadioButton size='40' /> */}
+                    {/* <CircleSolid size='40' /> */}
+                </div>
             </div>
         </>
     );
