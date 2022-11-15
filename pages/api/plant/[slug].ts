7c9b9e4b31664b898115c77b5166abe3f177d626
Bobby van Rijn <bobbyvanrijn@gmail.com>
2022-11-15 12:12:52 +0100

    fix: to typescript

---
+++ pages/api/plant/[slug].ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+    message: string,
+    plant: object | null
+}
+
+import { plants } from '../../../content/plants.js';
+
+export default function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    const { query } = req
+    const { slug } = query
+    const filtered = plants.filter((plant) => plant.slug === slug)
+
+    return filtered.length > 0
+        ? res.status(200).json({ 
+            message: 'Ok',
+            plant: filtered[0] 
+        })
+        : res.status(404).json({ 
+            message: `Plant with slug: ${slug} not found.`,
+            plant: null
+        })
+}
