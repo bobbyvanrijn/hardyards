654c467bc9d798bfd03d944d82af45b2203e3ba8
Bobby van Rijn <bobbyvanrijn@gmail.com>
2022-12-21 08:27:56 +0100

    fix: Meta bullets

--- components/Meta.js
+++ components/Meta.js
@@ -2,24 +2,24 @@ import styles from './Meta.module.css';
 
 export default function Meta(props) {
     return (
-        <div
-            { ...props }
-        >
+        <div { ...props }>
             <div className={styles.meta}>
-            { 
-                props.items.map((item, index) =>
-                    <div
-                        className={styles.meta__item}
-                        key={index}
-                    >
-                        {item.title !== undefined &&
-                            <div className='trim-both trim-both--secondary'>
-                                { item.value }
-                            </div>
-                        }
-                    </div>
-                )
-            }
+                <div className={styles['meta__container']}>
+                { 
+                    props.items.map((item, index) =>
+                        <div
+                            className={styles.meta__item}
+                            key={index}
+                        >
+                            {item.title !== undefined &&
+                                <div className='trim-both trim-both--secondary'>
+                                    { item.value }
+                                </div>
+                            }
+                        </div>
+                    )
+                }
+                </div>
             </div>
         </div>
     )
