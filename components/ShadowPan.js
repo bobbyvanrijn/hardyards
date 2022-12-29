a3394ea601b74f5611518e27bb576508bb0bba34
Bobby van Rijn <bobbyvanrijn@gmail.com>
2022-12-29 10:56:45 +0100

    feature: Simplify panner

--- components/ShadowPan.js
+++ components/ShadowPan.js
@@ -1,64 +1,38 @@
 import { useState, useEffect } from 'react';
-import { useEventListener, useIsClient, useIsomorphicLayoutEffect } from 'usehooks-ts';
-
 import { motion } from 'framer-motion';
 import styles from './ShadowPan.module.css';
 
 export default function ShadowPan(props) {
     const [activeElement, setActiveElement] = useState(null);
-    const [elementDimensions, setElementDimensions] = useState([]);
-
-    const keys = Object.keys(props.children);
 
     useEffect(() => {
         setActiveElement(props.activeChild);
     }, [props.activeChild]);
 
-    const dimensionHandler = () => {
-        let dimensions = [];
-        for (let i = 0; i < keys.length; i++) {
-            const id = keys[i];
-            const element = document.getElementById(id);
-
-            const width = element.offsetWidth;
-            const height = element.offsetHeight;
-            const x = element.offsetLeft;
-            const y = element.offsetTop;
-
-            dimensions[i] = {x, y, width, height}
-        }
-
-        setElementDimensions(dimensions);
-    }
-
-    useEventListener('resize', dimensionHandler)
-
-    useIsomorphicLayoutEffect(() => {
-        dimensionHandler();
-    }, []);
 
     return <>
         {
             !!props.children && props.children.map((item, index) => {
                 return (
                     <div
+                        className={styles['shadow-pan__wrapper']}
                         id={index}
                         key={index}
                         onClick={() => setActiveElement(index)}
                     >
                         {item}
+                        { index === activeElement ?
+                            <motion.div
+                                className={styles['shadow-pan__panner']}
+                                layoutId='panner'
+                            />
+                            :
+                            null
+                        }
                     </div>
                 );
             })
         }
 
-        <motion.div
-            layout
-            className={styles['shadow-pan__panner']}
-            style={{
-                left: elementDimensions[activeElement]?.x,
-                width: elementDimensions[activeElement]?.width,
-            }}
-        />
     </>
 }
