47982e463b5d7efd8ea01d53def859f2ecfcf174
Bobby van Rijn <bobbyvanrijn@gmail.com>
2022-12-30 10:34:53 +0100

    Fix: footer tweaks

--- components/Footer.js
+++ components/Footer.js
@@ -6,12 +6,9 @@ export default function Footer() {
     return (
         <footer className={styles.footer}>
             <GridLayout>
-                <div grid-x='1/3'><h3>Hard Yards</h3></div>
                 <div grid-x='1a' grid-x-sm='1'>
-                    Links
                     <Navigation />
                 </div>
-                <>Test</>
             </GridLayout>
         </footer>
     );
